54829484c8c68b8bba0683efa2a39b7696e3ac16
Bhushan Hiwase <bhushiwase@gmail.com>
2020-06-16 12:44:24 -0700

    Script to plot and monitor the price trends

---
+++ Price_Plot.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+
+y = []
+x = []
+
+with open("price_logs.csv", "r") as file:
+    for i in file:
+        data1 = file.read()
+        k1 = data1.split("\n")
+
+        for item in k1:
+            data2 = item.split(",")
+            if len(data2) == 4:
+                x.append(data2[0])
+                y.append(float(data2[2]))
+
+plt.xlabel("Date")
+plt.ylabel("Price")
+
+plt.bar(x, y)
+
+plt.show()
+
